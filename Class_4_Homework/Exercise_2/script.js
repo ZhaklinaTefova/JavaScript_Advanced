d7912498290ccc60b270e9726c068f9b7725c817
ZhaklinaTefova <119459579+ZhaklinaTefova@users.noreply.github.com>
2023-02-05 12:30:09 +0000

    Update script.js

--- Class_4_Homework/Exercise_2/script.js
+++ Class_4_Homework/Exercise_2/script.js
@@ -23,12 +23,30 @@ body.appendChild(button);
 let ul = document.createElement("ul");
 body.appendChild(ul);
 
-let countVowels = (str) => {
-    let vowels = str.match(/[aeiou]/gi);
-    return vowels === null ? `Your word has no vowels` : `Your word ${str} has ${vowels.length} vowels!`;
-}
-console.log(countVowels("aaa"));
+// // Regex:
+// let countVowels = (str) => {
+//     let vowels = str.match(/[aeiou]/gi);
+//     return vowels === null ? `Your word has no vowels` : `Your word ${str} has ${vowels.length} vowels!`;
+// }
+// // test the function
+// console.log(countVowels("aaa"));
 
+// LOOP
+let vowels = ["a","e","u","o","i"];
+
+let countVowels = (word) => {
+    count = 0;
+    for(let vowel of word) {
+        if(vowels.includes(vowel.toLowerCase())){
+            count++
+        }
+    } return `Your word ${word} contain ${count} vowels.`
+}
+// test the function
+console.log(countVowels("aaa"));
+console.log(countVowels("a"));
+
+// event click on a button
 button.addEventListener("click", () => {
     if (input.value === "") {
         alert("Please provide a word");
@@ -45,5 +63,3 @@ let clearInput = () => { input.value = "" };
 // let isNumber = value => { return !isNaN(value) && typeof value === 'number' }
 // console.log(isNumber(3));
 // console.log(isNumber("d"));
-
-
